517831f8c8005fbd7ce6a34ea7d590efc5f2514e
Facundo <facundo.vel99@gmail.com>
2025-04-04 23:07:03 -0300

    Se crea procedimiento para generar los datos para las pcs aleatoriamente

--- tp2_4.c
+++ tp2_4.c
@@ -13,6 +13,8 @@ typedef struct {
 
 char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
+void generacionDeDatos(compu pcs[], int cantidad);
+
 int main() {
 
     srand(time(NULL));
@@ -22,3 +24,11 @@ int main() {
     return 0;
 }
 
+void generacionDeDatos(compu pcs[], int cantidad){
+    for (int i = 0; i < cantidad ; i++) {
+        pcs[i].velocidad = 1 + rand() % 3;
+        pcs[i].anio = 2015 + rand() % 10;
+        pcs[i].cantidad_nucleos = 1 + rand() & 8;
+        pcs[i].tipo_cpu = tipos[rand() % 6];
+    }
+}
